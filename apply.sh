63e65f81af400d456d02eae088ad2704c26e30e2
Juuuuulie10 <65306784+Juuuuulie10@users.noreply.github.com>
2024-10-05 15:26:22 +0200

    new values

--- apply.sh
+++ apply.sh
@@ -1,10 +1,8 @@
 PROPERTY='
-gsm.sim.operator.numeric=23402
-gsm.sim.operator.iso-country=gb
-gsm.sim.operator.alpha=O2
-gsm.operator.numeric=23402
-gsm.operator.iso-country=gb
-gsm.operator.alpha=O2
+gsm.sim.operator.numeric=,20801
+gsm.sim.operator.iso-country=,fr
+gsm.operator.numeric=,20801
+gsm.operator.iso-country=,fr
 '
 
 for PROP in $PROPERTY; do
