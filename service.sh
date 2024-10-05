dbebd7ed4409a44bddec03867c7632ca7ced156f
Juuuuulie10 <65306784+Juuuuulie10@users.noreply.github.com>
2024-10-05 15:30:39 +0200

    Update service.sh

--- service.sh
+++ service.sh
@@ -7,12 +7,5 @@ while [ "$(getprop sys.boot_completed)" != 1 ]; do
     sleep 1
 done
 
-if [ -f $MODPATH/pm-clear ]; then
-  su -c 'pm clear com.google.android.gms'
-  su -c 'pm clear com.google.android.gsf'
-  su -c 'pm clear com.android.vending'
-  rm $MODPATH/pm-clear
-fi
-
 . $MODPATH/apply.sh
 crond -c $MODPATH/cron
