c711824fd1616d709987a0d874951e0f72dc2a35
Juuuuulie10 <65306784+Juuuuulie10@users.noreply.github.com>
2024-10-05 15:27:56 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,18 +1,4 @@
 ui_print '
 
-Following properties will be changed:
- * gsm.sim.operator.alpha: O2
- * gsm.sim.operator.numeric: 23402
- * gsm.sim.operator.iso-country: gb
- * gsm.operator.numeric: 23402
- * gsm.operator.iso-country: gb
- * gsm.operator.alpha: O2
-
-These packages will be cleared on next reboot:
- * com.google.android.gms
- * com.google.android.gsf
- * com.android.vending
+properties will be changed
 '
-
-# clearing Google Play services crashes Magisk Manager
-touch $MODPATH/pm-clear
